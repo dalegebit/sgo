6531aaa921edc5709fd826de352c309953e56f2f
dalegebit <dalegebit@gmail.com>
2018-09-01 00:37:25 -0700

    Install.sh tested and should be good for those who don't launch another shells as subprocesses

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+mkdir -p ~/.local/bin
+cp sgo.py ~/.local/bin/sgo
+chmod +x ~/.local/bin/sgo
+RC_FILE=~/.${SHELL##*[/-]}rc
+if [[ ! $PATH = *"~/.local/bin"* ]]; then
+    echo "export PATH=~/.local/bin:\$PATH" >> $RC_FILE
+fi
+source $RC_FILE
+

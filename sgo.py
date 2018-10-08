054247df85c8380b03d818e6e56ed70093c1df27
dalegebit <dalegebit@gmail.com>
2018-10-08 15:34:47 -0700

    Add -cpus option

--- sgo.py
+++ sgo.py
@@ -10,6 +10,7 @@ from subprocess import call
 parser = argparse.ArgumentParser()
 parser.add_argument('-p', '--ntasks', type=int, default=2, help="number of cores per node, default is 2")
 parser.add_argument('-n','--nodes', type=int, default=1, help="number of nodes, default is 1")
+parser.add_argument('-c', '--cpus', type=int, default=1, help="number of cpus per node, default is 1")
 parser.add_argument('-t', '--time', type=int, default=10, help="expected running time in seconds, default is 10")
 parser.add_argument('-o', '--output', type=str, default='', help="the *.out file, default is `execution_file_name`.out")
 parser.add_argument('-u', '--user', type=str, default='', help="user name, default is the current user")
@@ -38,6 +39,7 @@ with open(args.output_sl, 'w') as f:
     f.write("#!/bin/bash\n")
     f.write("#SBATCH --ntasks-per-node={}\n".format(args.ntasks))
     f.write("#SBATCH --nodes={}\n".format(args.nodes))
+    f.write("#SBATCH --cpus-per-task={}\n".format(args.cpus))
     f.write("#SBATCH --time={}:{}:{}\n".format(
         str(args.time // 3600).zfill(2),
         str(args.time // 60 % 60).zfill(2),

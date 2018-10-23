c92ecca69daa5630da32d48b69333c1a8895f9d3
dalegebit <dalegebit@gmail.com>
2018-10-22 18:01:43 -0700

    Fix bugs in install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ mkdir -p ~/.local/bin
 cp sgo.py ~/.local/bin/sgo
 chmod +x ~/.local/bin/sgo
 RC_FILE=~/.${SHELL##*[/-]}rc
-if [[ ! $PATH = *"~/.local/bin"* ]]; then
+if [[ ! $PATH == *$HOME"/.local/bin"* ]]; then
     echo "export PATH=~/.local/bin:\$PATH" >> $RC_FILE
 fi
 source $RC_FILE
